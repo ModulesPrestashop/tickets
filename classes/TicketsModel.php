b141a93d5f89e91d4e501f0381a44911e0a9e39a
jorgevrgs <jorgevargaslarrota@hotmail.com>
2015-09-23 12:16:22 -0500

    [*] MO Tickets : PSR-2 Updated

--- classes/TicketsModel.php
+++ classes/TicketsModel.php
@@ -26,31 +26,33 @@
 
 class TicketsModel extends ObjectModel
 {
-	public static function getCustomerThreadByIdCustomer($id_customer)
-	{
-		$sql = 'SELECT *
-			FROM '._DB_PREFIX_.'customer_thread
-			WHERE id_customer = '.(int)$id_customer;
+    public static function getCustomerThreadByIdCustomer($id_customer)
+    {
+        $sql = 'SELECT *
+            FROM '._DB_PREFIX_.'customer_thread
+            WHERE id_customer = '.(int)$id_customer;
 
-		return Db::getInstance()->executeS($sql);
-	}
+        return Db::getInstance()->executeS($sql);
+    }
 
-	public static function getIdCustomerThreadByIdAndToken($id_customer_thread, $token)
-	{
-		$context = Context::getContext();
-		return (int)Db::getInstance()->getValue('
-			SELECT cm.id_customer_thread FROM '._DB_PREFIX_.'customer_thread cm
-		WHERE cm.id_customer_thread = '.(int)$id_customer_thread.'
-		AND cm.id_shop = '.(int)$context->shop->id.' AND token = \''.pSQL($token).'\'');
-	}
+    public static function getIdCustomerThreadByIdAndToken($id_customer_thread, $token)
+    {
+        $context = Context::getContext();
+        return (int)Db::getInstance()->getValue(
+            'SELECT cm.id_customer_thread FROM '._DB_PREFIX_.'customer_thread cm
+                WHERE cm.id_customer_thread = '.(int)$id_customer_thread.'
+            AND cm.id_shop = '.(int)$context->shop->id.' AND token = \''.pSQL($token).'\''
+        );
+    }
 
-	public static function getLastMessage($id_customer_thread)
-	{
-		$context = Context::getContext();
-		return Db::getInstance()->getValue('
-			SELECT cm.message FROM '._DB_PREFIX_.'customer_message cm
-		LEFT JOIN '._DB_PREFIX_.'customer_thread cc on (cm.id_customer_thread = cc.id_customer_thread)
-		WHERE cc.id_customer_thread = '.(int)$id_customer_thread.' AND cc.id_shop = '.(int)$context->shop->id.'
-		ORDER BY cm.date_add DESC');
-	}
+    public static function getLastMessage($id_customer_thread)
+    {
+        $context = Context::getContext();
+        return Db::getInstance()->getValue(
+            'SELECT cm.message FROM '._DB_PREFIX_.'customer_message cm
+                LEFT JOIN '._DB_PREFIX_.'customer_thread cc on (cm.id_customer_thread = cc.id_customer_thread)
+            WHERE cc.id_customer_thread = '.(int)$id_customer_thread.' AND cc.id_shop = '.(int)$context->shop->id.'
+            ORDER BY cm.date_add DESC'
+        );
+    }
 }
--- tickets.php
+++ tickets.php
@@ -24,63 +24,66 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
-if (!defined('_PS_VERSION_'))
-	exit;
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
 
 class Tickets extends Module
 {
-	protected $config_form = false;
+    protected $config_form = false;
 
-	public function __construct()
-	{
-		$this->name = 'tickets';
-		$this->tab = 'front_office_features';
-		$this->version = '1.0.0';
-		$this->author = 'Jorge Vargas';
-		$this->need_instance = 0;
-		$this->controllers = array('support');
-		/**
-		 * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
-		 */
-		$this->bootstrap = true;
+    public function __construct()
+    {
+        $this->name = 'tickets';
+        $this->tab = 'front_office_features';
+        $this->version = '1.0.0';
+        $this->author = 'Jorge Vargas';
+        $this->need_instance = 0;
+        $this->controllers = array('support');
+        /**
+         * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
+         */
+        $this->bootstrap = true;
 
-		parent::__construct();
+        parent::__construct();
 
-		$this->displayName = $this->l('Customer Thread');
-		$this->description = $this->l('Extend features of customer thread by including ticket history in front-office');
-		$this->confirmUninstall = $this->l('Are you sure you want to uninstall this module?');
-		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
-		$this->ticket_status = array(
-			'open' => $this->l('Open'),
-			'pending1' => $this->l('Pending internal'),
-			'pending2' => $this->l('Awating answer'),
-			'closed' => $this->l('Closed')
-		);
-	}
+        $this->displayName = $this->l('Customer Thread');
+        $this->description = $this->l(
+            'Extend features of customer thread by including ticket history in front-office'
+        );
+        $this->confirmUninstall = $this->l('Are you sure you want to uninstall this module?');
+        $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+        $this->ticket_status = array(
+            'open' => $this->l('Open'),
+            'pending1' => $this->l('Pending internal'),
+            'pending2' => $this->l('Awating answer'),
+            'closed' => $this->l('Closed')
+        );
+    }
 
-	/**
-	 * Don't forget to create update methods if needed:
-	 * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
-	 */
-	public function install()
-	{
-		return parent::install() &&
-			$this->registerHook('displayCustomerAccount') &&
-			$this->registerHook('displayMyAccountBlock');
-	}
+    /**
+     * Don't forget to create update methods if needed:
+     * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
+     */
+    public function install()
+    {
+        return parent::install() &&
+            $this->registerHook('displayCustomerAccount')
+            && $this->registerHook('displayMyAccountBlock');
+    }
 
-	public function uninstall()
-	{
-		return parent::uninstall();
-	}
+    public function uninstall()
+    {
+        return parent::uninstall();
+    }
 
-	public function hookDisplayCustomerAccount()
-	{
-		return $this->display(__FILE__, 'customer-account.tpl');
-	}
+    public function hookDisplayCustomerAccount()
+    {
+        return $this->display(__FILE__, 'customer-account.tpl');
+    }
 
-	public function hookDisplayMyAccountBlock()
-	{
-		/* Place your code here. */
-	}
+    public function hookDisplayMyAccountBlock()
+    {
+        /* Place your code here. */
+    }
 }
